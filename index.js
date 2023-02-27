b91e0cab84782dedb3a862bf1a43001d82fe1d33
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-02-27 16:16:36 +0300

    Dev:- Created Files

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const hello = "Hello World";
-console.log(hello)
+// fs Stands for file system
+const fs = require('fs')
+
+// Creating new files 
+const textIn = fs.readFileSync("./txt/input.txt",'utf-8');
+console.log(textIn)
+
