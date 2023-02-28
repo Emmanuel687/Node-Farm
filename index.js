a5f4f3f30df0074dcc12c5452c02c7d647a70772
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-02-28 13:37:18 +0300

    Error Handling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // fs Stands for file system
 const fs = require("fs");
+const http = require("http")
 
 // Creating new files
 // const textIn = fs.readFileSync("./txt/input.txt",'utf-8');
@@ -12,12 +13,20 @@ const fs = require("fs");
 // console.log('File written!')
 
 // Creating Files Asyhchrously
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    if(err){console.log('Error')}
-    console.log(data2);
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     if(err){console.log('Error')}
+//     console.log(data2);
     
-  });
-});
+//   });
+// });
 
 // fs.writeFile("./txt/")
+
+const server = http.createServer((req,res)=>{
+    res.end('Hello From the server')
+})
+
+server.listen(8000, "127.0.8.1", ()=>{
+    console.log("Listening to request on port 8000")
+})
