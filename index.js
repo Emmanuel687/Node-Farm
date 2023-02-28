41fb246717dc158a260e05dbb8e7d688f151ec9f
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-02-28 12:16:08 +0300

    Reading data

--- index.js
+++ index.js
@@ -2,15 +2,18 @@
 const fs = require('fs')
 
 // Creating new files 
-const textIn = fs.readFileSync("./txt/input.txt",'utf-8');
-console.log(textIn)
+// const textIn = fs.readFileSync("./txt/input.txt",'utf-8');
+// console.log(textIn)
 
-const textOut = `This is what we know about the avacado ${textIn}.\n Created on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt",textOut)
+// Writing Files Synchrously
+// const textOut = `This is what we know about the avacado ${textIn}.\n Created on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt",textOut)
 
-console.log('File written!')
+// console.log('File written!')
 
-
+// Creating Files Asyhchrously
 fs.readFile('./txt/start.txt','utf-8',(err,data)=>{
     console.log(data)
 })
+
+// fs.writeFile("./txt/")
